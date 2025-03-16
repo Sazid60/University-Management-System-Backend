7500cbbbaf19e3c136d909cfb2d0d4aefbf26f9c
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-03-16 09:46:06 +0600

    Routing System Organized for Efficiency

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 const app: Application = express();
 
 // parser
@@ -13,14 +12,13 @@ app.use(express.json());
 app.use(cors());
 
 // application Routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1/', router);
 
-const getAController = (req: Request, res: Response) => {
+const test = (req: Request, res: Response) => {
   res.send('Connected');
 };
 
-app.get('/', getAController);
+app.get('/', test);
 
 // @ts-expect-error
 app.use(globalErrorHandler);
