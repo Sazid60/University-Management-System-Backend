3674713a484b204610cafdb29246ce12d9575ad0
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-03-19 11:08:26 +0600

    Handling Logical Validation of Academic Semester

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -37,8 +37,31 @@ const academicSemesterSchema = new Schema<TAcademicSemester>(
     timestamps: true,
   },
 );
+// same year same semester can not be created again we have to take in our mind
+academicSemesterSchema.pre('save', async function (next) {
+  // this next is mongoose's next
+  const isSemesterExist = await AcademicSemester.findOne({
+    year: this.year,
+    name: this.name,
+  });
+
+  if (isSemesterExist) {
+    throw new Error('Semester Is Already exist');
+  }
+  next();
+});
 
 export const AcademicSemester = model<TAcademicSemester>(
   'AcademicSemester',
   academicSemesterSchema,
 );
+
+// Name Year
+//2030 Autumn => Created
+// 2031 Autumn
+//2030 Autumn => XXX
+//2030 Fall => Created
+
+// Autumn 01
+// Summar 02
+// Fall 03
--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -2,6 +2,32 @@ import { TAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+  //  semester Name --> semester code
+
+  // type TAcademicSemesterCodeMapper = {
+  //   Autumn: '01',
+  //   Summer: '02',
+  //   Fall: '03',
+  // }
+
+  //  we can use dynamically mapped type
+  //  this is used so that if further other semester added it automatically or dynamically gets the value
+  type TAcademicSemesterCodeMapper = {
+    [key: string]: string;
+  };
+  const academicSemesterNameCodeMapper: TAcademicSemesterCodeMapper = {
+    Autumn: '01',
+    Summer: '02',
+    Fall: '03',
+  };
+  // (academicSemesterNameCodeMapper['Fall']!== payload.code)
+  // academicSemesterNameCodeMapper['Fall']=03 this will come from mapper
+  // this wo=ill come from my payload payload.code = "01"
+  // (academicSemesterNameCodeMapper['Fall']!== 01)
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid Semester Code');
+  }
+
   const result = await AcademicSemester.create(payload);
   return result;
 };
