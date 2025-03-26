44aaba1ea97a5224c952551f9a8222097081f462
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-03-26 12:37:15 +0600

    Convert Zod Error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,16 +1,13 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { ErrorRequestHandler } from 'express';
-import { ZodError } from 'zod';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = err.statusCode || 500;
   let message = err.message || 'Something Went Wrong';
-
-  type TErrorSource = {
-    path: string | number;
-    message: string;
-  }[];
 
   let errorSource: TErrorSource = [
     {
@@ -19,16 +16,42 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     },
   ];
 
+  // zod error handler
+  const handleZodError = (err: ZodError) => {
+    // handler organize the errors structure
+
+    // making error source since we are not sending the zod error directly
+    // we ar doing map since multiple issue can come
+    const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue.path.length - 1],
+        //we are doing this since last index of path object shows the exact path
+        message: issue.message,
+      };
+    });
+    return {
+      statusCode,
+      message: 'Validation Error',
+      errorSource,
+    };
+  };
   // To checking class, subclass or instance we have to use instanceof operator
+  //  we are detecting here that this is a zod error
   if (err instanceof ZodError) {
-    statusCode = 400;
-    message = 'Ami Zod Error';
+    // now we will send the error to the handler
+    const simplifiedError = handleZodError(err);
+    // console.log(simplifiedError);
+    //  now we will be doing over write
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSource = simplifiedError?.errorSource;
   }
 
   res.status(statusCode).json({
     success: false,
     message,
     errorSource,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
 
