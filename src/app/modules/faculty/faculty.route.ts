fbca2807e7bdce17060b745204df5a3bed66f4d1
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-04-06 11:24:39 +0600

    Updating faculty Mechanism Added

--- src/app/modules/faculty/faculty.route.ts
+++ src/app/modules/faculty/faculty.route.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 router.get('/', FacultyControllers.getAllFaculties);
 router.get('/:facultyId', FacultyControllers.getSingleFaculty);
 router.delete('/:facultyId', FacultyControllers.deleteFaculty);
+router.patch('/:facultyId', FacultyControllers.updateFaculty);
 
 export const FacultyRoutes = router;
