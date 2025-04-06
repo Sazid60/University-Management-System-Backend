e9d62dc3dfabba78446631950b5f5f132746a068
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-04-06 13:09:58 +0600

    Admin all Routes Complete Including Dynamic Id Generation and All

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,6 +4,7 @@ import { UserController } from './user.controller';
 import { createStudentValidationSchema } from '../student/student.validation';
 import validateRequest from '../../middlewares/validateRequest';
 import { createFacultyValidationSchema } from '../faculty/faculty.validation';
+import { createAdminValidationSchema } from '../admin/admin.validation';
 
 const router = express.Router();
 
@@ -18,6 +19,11 @@ router.post(
   '/create-faculty',
   validateRequest(createFacultyValidationSchema),
   UserController.createFaculty,
+);
+router.post(
+  '/create-admin',
+  validateRequest(createAdminValidationSchema),
+  UserController.createAdmin,
 );
 
 export const UserRoutes = router;
